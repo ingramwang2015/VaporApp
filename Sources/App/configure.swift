b64200e2f12d96c31fd91e05efec27c1bdaea49d
ingramwang <ingramwang2015@gmail.com>
2018-10-09 09:28:18 +0800

    update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,11 +18,28 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     /// Register the configured PostgreSQL database to the database config.
+    let databaseName : String
+    let port : Int
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        port = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        port = 5432
+    }
+    
     var databases = DatabasesConfig()
     let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor")
+        hostname: hostname,
+        port: port,
+        username: username,
+        database: databaseName,
+        password: password)
+    
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -9,24 +9,31 @@ final class UserTests: XCTestCase {
         let expectedName = "Alice"
         let expectedUsername = "alice"
         
-        var config = Config.default()
-        var services = Services.default()
-        var env = Environment.testing
-        try App.configure(&config, &env, &services)
-        let app = try Application(config: config, environment: env, services: services)
-        try boot(app)
+        let revertEnvironmentArgs = ["vapor", "revert", "--all", "-y"]
+        var revertConfig = Config.default()
+        var revertServices = Services.default()
+        var revertEnv = Environment.testing
         
-        let conn = try app.newConnection(to: .psql).wait()
+        revertEnv.arguments = revertEnvironmentArgs
+        
+        try App.configure(&revertConfig, &revertEnv, &revertServices)
+        
+        let revertApp = try Application(config: revertConfig, environment: revertEnv, services: revertServices)
+        try boot(revertApp)
+        
+        try revertApp.asyncRun().wait()
+        
+        let conn = try revertApp.newConnection(to: .psql).wait()
         
         let user = User(name: expectedName, username: expectedUsername)
         let savedUser = try user.save(on: conn).wait()
         
         _ = try User(name: "Luke", username: "lukes").save(on: conn).wait()
         
-        let responder = try app.make(Responder.self)
+        let responder = try revertApp.make(Responder.self)
         
         let request = HTTPRequest(method: .GET, url: URL(string: "/api/users")!)
-        let wrappedRequest = Request(http: request, using: app)
+        let wrappedRequest = Request(http: request, using: revertApp)
         
         let reponse = try responder.respond(to: wrappedRequest).wait()
         
